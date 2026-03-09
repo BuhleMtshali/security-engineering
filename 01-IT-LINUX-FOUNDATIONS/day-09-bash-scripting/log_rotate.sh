687340f3601039e62f664319b917501abc6ff51f
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-09 19:32:34 +0200

    updated the log file

--- 01-IT-LINUX-FOUNDATIONS/day-09-bash-scripting/log_rotate.sh
+++ 01-IT-LINUX-FOUNDATIONS/day-09-bash-scripting/log_rotate.sh
@@ -1,2 +1,9 @@
 # Step 1: Pick a Log to Manage
 # For a simple mini project, boot.log or dpkg.log is perfect because they’re plain logs and already exist.
+
+/var/log/boot.log
+
+# 🔎 Step 2: Inspect the Log
+
+# First look at the log files
+cat /var/log/boot.log
