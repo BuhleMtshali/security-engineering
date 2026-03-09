248147074847c012e4c419741dfea041974e9cc6
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-09 19:53:44 +0200

    updated the log rotate

--- 01-IT-LINUX-FOUNDATIONS/day-09-bash-scripting/log_rotate.sh
+++ 01-IT-LINUX-FOUNDATIONS/day-09-bash-scripting/log_rotate.sh
@@ -7,3 +7,26 @@
 
 # First look at the log files
 cat /var/log/boot.log
+
+# Or just the latest entries:
+tail /var/log/boot.log
+
+# This shows what the log records.
+
+
+# ⚙️ Step 3: Create Your Own Logrotate Rule
+# Logrotate configs live here:
+/etc/logrotate.d/
+
+# Create a custom rule:
+sudo nano /etc/logrotate.d/boot-log-mini
+
+# Add this configuration:
+
+/var/log/boot.log {
+    weekly
+    rotate 4
+    compress
+    missingok
+    notifempty
+}
