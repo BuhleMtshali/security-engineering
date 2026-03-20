62df57fab15d346744010c15776cbb299bca8205
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-20 10:06:34 +0200

    updatted the log file

--- 01-IT-LINUX-FOUNDATIONS/day-14-python-basics-1/log_ip_counter.py
+++ 01-IT-LINUX-FOUNDATIONS/day-14-python-basics-1/log_ip_counter.py
@@ -2,4 +2,15 @@
 
 log_file = "/var/log/syslog"
 
-print(log_file)
+# CREATING A DICTIONARY
+ip_counts = {}
+
+# WE NEED TO OPEN THE FILE IN READING MODE
+with open(log_file, "r") as file:
+    for line in file:
+        parts = line.split()
+        ip = parts[0]
+        if ip in ip_counts:
+            ip_counts[ip] += 1
+        else:
+            ip_counts[ip] = 1
